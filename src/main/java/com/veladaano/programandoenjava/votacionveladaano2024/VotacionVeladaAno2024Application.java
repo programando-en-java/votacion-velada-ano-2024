98ea4a15c7f11e7c4cdbf62ebef4bbdc4b470e76
Ricardo Romero <ricardoromebeni@gmail.com>
2024-03-19 19:52:35 +0100

    Proyecto generado
    Estructura del proyecto generado

---
+++ src/main/java/com/veladaano/programandoenjava/votacionveladaano2024/VotacionVeladaAno2024Application.java
@@ -0,0 +1,13 @@
+package com.veladaano.programandoenjava.votacionveladaano2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionVeladaAno2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionVeladaAno2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/veladaano/programandoenjava/votacionveladaano2024/VotacionVeladaAno2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veladaano.programandoenjava.votacionveladaano2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacionVeladaAno2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
