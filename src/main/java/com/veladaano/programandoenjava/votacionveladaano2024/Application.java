4d7d3e6355108305e1dbca764f3267dd54a7f673
Luiz Segundo <simoes.segundo@gmail.com>
2024-04-23 23:41:12 -0300

    Remove unnecessary annotation

--- src/main/java/com/veladaano/programandoenjava/votacionveladaano2024/Application.java
+++ src/main/java/com/veladaano/programandoenjava/votacionveladaano2024/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.veladaano.programandoenjava.votacionveladaano2024"})
 public class Application {
 
     public static void main(String[] args) {
